693573ddad7e519af1216ee7775e7564d2a29a8a
taohua <liuyong@bullyun.com>
2018-01-20 14:36:58 +0800

    modify

--- src/pages/address/address.vue
+++ src/pages/address/address.vue
@@ -1,17 +1,48 @@
 <template>
     <div class="addressManage">
-        12345
+        <div class="info">
+            <div class="avatar"></div>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+        <div class="content">内容</div>
     </div>
 </template>
 
 <script>
     export default {
-        name: "address"
+        name: "address",
+        data(){
+            return{
+
+            }
+        }
     }
 </script>
 
 <style lang="less">
     .addressManage{
-
+        box-sizing: border-box;
+        *{box-sizing: border-box;}
+        border:1px solid red;
+        height: 1000px;
+        width:100%;
+        display: flex;
+        .info{
+            margin-right: 48px;
+            width:190px;
+            padding:5px;
+        }
+        .content{
+            flex:1;
+            border:1px solid red;
+        }
     }
 </style>

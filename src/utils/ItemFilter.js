114968eaa930b080080f1529b30ee5553baca7d9
王海明 <wanghaiming@500mi.com>
2018-01-23 15:20:52 +0800

    fixed

--- src/utils/ItemFilter.js
+++ src/utils/ItemFilter.js
@@ -5,7 +5,7 @@ export default {
             this.filter(item)
         }else if(typeof(item) === 'Array'){
             item.forEach((value,index,array)=>{
-              this.filter(value)
+                array[index] = this.filter(value)
             })
         }else{
             throw new Error("商品数据类型出错")
@@ -15,5 +15,8 @@ export default {
 
     filter(item){
         item.price = (item.price/100).toFixed(2);
+        item.cdate = item.cdate
+
+        return item;
     },
 }

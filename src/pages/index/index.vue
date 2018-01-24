0a8d58ebbc56aa2a72edb760954e29df6c665b15
huqin <huqin@huntlee.cn>
2018-01-24 15:01:28 +0800

    修改

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -57,7 +57,6 @@
                   <li  v-for="item in itemlist.items" :key="item.sin">
                       <a :href="'/detail#/?id=' + item.id">
                           <div class="img" :style="{backgroundImage : 'url(' + 'http://res.500mi.com/item/'+item.url+')'}"></div>
-                      </a>
                           <div class="item-spec">
                               <p class="line-one">{{item.brand}}</p>
                               <p class="line-two">{{item.item_name}}</p>
@@ -67,7 +66,7 @@
                                  <p v-ltsLoginShow:true class="price">${{item.price }}</p>
                               </div>
                           </div>
-
+                      </a>
                   </li>
               </ul>
             </div>
@@ -259,12 +258,12 @@ import homeService from '@/services/HomeService.js'
                     padding-left: 11px;
                 }
                 .i::before{
-                    content:'';
-                    width:3px;
+                    content: '';
+                    width: 3px;
                     height: 14px;
                     background: #3b85ff;
                     position: absolute;
-                    top:5px;
+                    top: 5px;
                     left: 0px;
                 }
                 .more{
@@ -287,7 +286,7 @@ import homeService from '@/services/HomeService.js'
                     background-color: #ffffff;
                     text-align: center;
                     min-width: 200px;
-                    width: 24%;
+                    flex:1;
                     margin-bottom: 12px;
                     margin-right: 1%;
                 }
@@ -301,6 +300,19 @@ import homeService from '@/services/HomeService.js'
             .title{
                 margin-top: 14px;
                 color:#737373;
+                .i{
+                    position: relative;
+                    padding-left: 11px;
+                }
+                .i::before{
+                    content: '';
+                    width: 3px;
+                    height: 14px;
+                    background: #3b85ff;
+                    position: absolute;
+                    top: 5px;
+                    left: 0px;
+                }
             }
             .best-sellers{
                 margin-top: 15px;
@@ -345,9 +357,8 @@ import homeService from '@/services/HomeService.js'
                     color: #a3a3a3;
                 }
                 .line-four {
-                    width: 266px;
                     border-top: 1px solid #f2f2f2;
-                    margin: 0 auto;
+                    margin: 0 12px;
                 }
                 p{
                     overflow: hidden;

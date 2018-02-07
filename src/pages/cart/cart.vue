6fe93decf89d204f4f3b99bde932deb030c69ce0
taohua <liuyong@bullyun.com>
2018-02-07 21:21:22 +0800

    modify

--- src/pages/cart/cart.vue
+++ src/pages/cart/cart.vue
@@ -3,8 +3,8 @@
         <div class="header">
             <a href="/"><img src="../../assets/img/LOGO_html.png" alt="LTS-logo"></a>
             <el-steps :active=active align-center finish-status="success">
-                <el-step title="我的进货单"></el-step>
-                <el-step title="填写/确认订单信息"></el-step>
+                <el-step title="购物车"></el-step>
+                <el-step title="确认订单信息"></el-step>
                 <el-step title="订单提交"></el-step>
                 <el-step title="支付"></el-step>
                 <el-step title="等待收货"></el-step>

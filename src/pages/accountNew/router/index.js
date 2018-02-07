3a90ff9c878eedafd06f515f21048d6e415831d2
huqin <huqin@huntlee.cn>
2018-02-07 22:36:07 +0800

    注册页面提交

---
+++ src/pages/accountNew/router/index.js
@@ -0,0 +1,27 @@
+import Router from 'vue-router'
+import Register from '../components/register.vue'
+import Reset from '../components/reset.vue'
+import SignupFinish from '../components/signupFinish.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+      {
+          path: '/',
+          component: Register
+      },
+    {
+      path: '/register',
+      component: Register
+    },
+      {
+          path: '/reset',
+          component: Reset
+      },
+      {
+          path: '/signupFinish',
+          component: SignupFinish
+      },
+  ]
+})

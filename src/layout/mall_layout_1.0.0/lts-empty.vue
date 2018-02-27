4eb68131f995c01f490a31db9d7cf9c77f48b93e
taohua <liuyong@bullyun.com>
2018-02-27 14:40:51 +0800

    购物车表格宽度bug

---
+++ src/layout/mall_layout_1.0.0/lts-empty.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "lts-empty"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/layout/mall_layout_1.0.0/lts-header-simple.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="simpleHead">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "lts-head-simple"
+    }
+</script>
+
+<style lang="less">
+    .simpleHead{
+
+    }
+</style>

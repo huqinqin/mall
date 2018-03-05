6f484163af31fccd5babea385b3004b477759231
王海明 <wanghaiming@500mi.com>
2018-03-05 19:51:56 +0800

    fixed

--- src/lang/cn.js
+++ src/lang/cn.js
@@ -1,11 +1,4 @@
 export default {
-    name : '123',
-    title: '工作台',
-    menu: {
-      product : '商品管理',
-      goods: '商品管理',
-      order: '订单管理'
-    },
     comHeader: {
         headerImmediatelyLog: '立即登录',
         headerWelcom: '欢迎',

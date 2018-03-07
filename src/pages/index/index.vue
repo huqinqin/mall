8ffee77410a4ab06f5b22f040009b7b65c6ab33a
huqin <huqin@huntlee.cn>
2018-03-07 10:15:29 +0800

    首页“更多”的提交

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -77,8 +77,10 @@
               <span>{{itemlist.name}}</span>
             </div>
             <div class="more">
-              <span>{{$t("main.detail.info.mainInMore")}}</span>
-              <i class="iconfont icon-shangyiye-copy-copy rotate"></i>
+                <a :href="itemlist.url">
+                    <span>{{$t("main.detail.info.mainInMore")}}</span>
+                    <i class="iconfont icon-shangyiye-copy-copy rotate"></i>
+                </a>
             </div>
           </div>
           <ul class="item-list-box">

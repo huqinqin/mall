64d25f08959b62fcf29c769e3cf227d408a95110
taohua <liuyong@bullyun.com>
2018-04-23 20:38:08 -0700

    login

--- src/common/components/lts-message.js
+++ src/common/components/lts-message.js
@@ -1,11 +1,12 @@
 import {Message} from 'element-ui'
 export default {
   show(opotion){
-    opotion = opotion ? opotion : {type : '',message:'用message组件你需要传{type:"类型"，message;"信息""}'}
+    opotion = opotion ? opotion : {type :'', message:'用message组件你需要传{type:"类型"，message;"信息""}', class:''}
     Message({
       showClose: true,
       message: opotion.message,
       type : opotion.type,
-    })
+      customClass:opotion.class
+  })
   }
 }

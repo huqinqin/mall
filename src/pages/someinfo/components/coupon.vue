2047550f09b544de19d21af7f9a21b6b31034bae
taohua <liuyong@bullyun.com>
2018-05-17 18:44:09 -0700

    coupon

--- src/pages/someinfo/components/coupon.vue
+++ src/pages/someinfo/components/coupon.vue
@@ -1,10 +1,69 @@
 <template>
-    <div class="coupon-info">1234</div>
+    <div class="coupon-info">
+        <el-tabs v-model="activeName" type="card" @tab-click="handleClick">
+            <el-tab-pane label="Available Coupons" name="first">
+                <ul>
+                    <li v-for="bonus in ableBonusList">{{bonus.id}}</li>
+                </ul>
+            </el-tab-pane>
+            <el-tab-pane label="Failure" name="second">
+                <ul>
+                    <li v-for="bonus in unableBonusList">{{bonus.id}}</li>
+                </ul>
+            </el-tab-pane>
+        </el-tabs>
+    </div>
 </template>
 
 <script>
+    import checkService from '@/services/CheckService'
+    import timeService from '@/services/TimeService'
     export default {
-        name: "coupon"
+        name: "coupon",
+        data(){
+            return{
+                activeName:'first',
+                unableBonusList:[],
+                ableBonusList:[],
+                now:''
+            }
+        },
+        methods:{
+            handleClick(tab, event) {
+                // console.log(tab, event);
+            },
+            getNowTime(){
+                timeService.getTimeAndZone().then(time => {
+                    this.now = new Date(time.current_time).getTime()
+                    this.getBonusList()
+                },err => {
+                    this.$ltsMessage({type:'error',message:err.error_message})
+                })
+
+            },
+            getBonusList(){
+                checkService.checkInfo().then((data) => {
+                    data.data.acc_books.forEach(t => {
+                        if(t.subject === 2010102 && t.bonus){
+                            t.bonus.datalist.forEach(item => {
+                                debugger
+                                if(item.end_time < this.now){
+                                    this.unableBonusList.push(item)
+                                }else{
+                                    this.ableBonusList.push(item)
+                                    this.unableBonusList.push(item)
+                                }
+                            })
+                        }
+                    })
+                },(msg) => {
+                    this.$ltsMessage({type:'error',message:msg.error_message})
+                })
+            }
+        },
+        mounted(){
+            this.getNowTime()
+        }
     }
 </script>
 
